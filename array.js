e280bc6456d4f88be24711443f6b5cc3ce2feb52
garbagemanonfire <nilesvm@gmail.com>
2013-11-09 15:29:13 -0800

    submission for challenge

---
+++ array.js
@@ -0,0 +1,48 @@
+//  //Forward
+ 
+//  var dogs = ["Fido", "Rex", "Maggie", "Pumpkin"];
+
+//         for (var i = 0; i <= dogs.length; i++) {
+//           console.log(dogs[i]);
+//         }
+
+//         // Fido
+//         // Rex
+//         // Maggie
+//         // Pumpkin
+
+
+
+
+// //Reverse
+//  var dogs = ["Fido", "Rex", "Maggie", "Pumpkin"];
+
+//         for (var i = dogs.length -1; i >= 0; i--) {
+//           console.log(dogs[i]);
+//         }
+
+//         // Fido
+//         // Rex
+//         // Maggie
+//         // Pumpkin
+
+// //Random dog name
+//  var dogs = ["Fido", "Rex", "Maggie", "Pumpkin"];
+
+//         for (var i = 0; i <= dogs.length; i(3-math.random) {
+//           console.log(dogs[i]);
+//         }
+
+
+
+
+
+var testScores = [80, 82, 92, 67, 97, 73, 74];
+
+testScores.forEach(function (item, index) {
+              console.log("Test score at index #" + index + ": " + item)
+            });
+
+
+
+
